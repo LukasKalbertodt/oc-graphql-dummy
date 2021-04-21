edaa0cfcd7c94e6067bfb87cc3d97ea01cbc7837
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-04-21 21:22:46 +0200

    Fix indentation

--- src/main/java/org/opencast/graphql/dummy/Event.java
+++ src/main/java/org/opencast/graphql/dummy/Event.java
@@ -3,24 +3,24 @@ package org.opencast.graphql.dummy;
 import java.util.Arrays;
 
 public class Event {
-  public String id;
-  public String title;
-  public String description;
-  public float duration;
-  private String seriesID;
+    public String id;
+    public String title;
+    public String description;
+    public float duration;
+    public String seriesID;
 
-  public Event(String id, String title, String description, float duration, String seriesID) {
-    this.id = id;
-    this.title = title;
-    this.description = description;
-    this.duration = duration;
-    this.seriesID = seriesID;
-  }
+    public Event(String id, String title, String description, float duration, String seriesID) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.duration = duration;
+        this.seriesID = seriesID;
+    }
 
-  public Series getSeries() {
-    return Arrays.stream(Data.series)
-        .filter(series -> series.id.equals(this.seriesID))
-        .findFirst()
-        .orElse(null);
-  }
+    public Series getSeries() {
+        return Arrays.stream(Data.series)
+            .filter(series -> series.id.equals(this.seriesID))
+            .findFirst()
+            .orElse(null);
+    }
 }
--- src/main/java/org/opencast/graphql/dummy/Series.java
+++ src/main/java/org/opencast/graphql/dummy/Series.java
@@ -1,13 +1,13 @@
 package org.opencast.graphql.dummy;
 
 public class Series {
-  public String id;
-  public String name;
-  public String description;
+    public String id;
+    public String name;
+    public String description;
 
-  public Series(String id, String name, String description) {
-    this.id = id;
-    this.name = name;
-    this.description = description;
-  }
+    public Series(String id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
 }
