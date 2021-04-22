5595521622b665a5ea4f4e859517c552c76a6c63
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-04-22 12:51:28 +0200

    Add tracks to events

--- src/main/java/org/opencast/graphql/dummy/Data.java
+++ src/main/java/org/opencast/graphql/dummy/Data.java
@@ -6,37 +6,60 @@ public class Data {
             "My Neighbor Totoro",
             "A cute movie about childhood",
             5280,
-            "76543210-abcd-abcd-abcd-000000000001"),
+            "76543210-abcd-abcd-abcd-000000000001",
+            new Event.Track[] {
+                new Event.Track("video/x-matroska", "https://example.com/totoro.mkv", 24, 1920, 1080)
+            }),
         new Event("01234567-abcd-abcd-abcd-000000000002",
             "Spirited Away",
             "Animated fantasy movie with a strong theme of environmentalism",
             7500,
-            "76543210-abcd-abcd-abcd-000000000001"),
+            "76543210-abcd-abcd-abcd-000000000001",
+            new Event.Track[] {
+                new Event.Track("video/x-matroska", "https://example.com/chihiro.mkv", 24, 1920, 1080)
+            }),
         new Event("01234567-abcd-abcd-abcd-000000000003",
             "Princess Mononoke",
             "Another masterpiece.",
             7980,
-            "76543210-abcd-abcd-abcd-000000000001"),
+            "76543210-abcd-abcd-abcd-000000000001",
+            new Event.Track[]  {
+                new Event.Track("video/x-matroska", "https://example.com/mononoke.mkv", 24, 1920, 1080)
+            }),
         new Event("01234567-abcd-abcd-abcd-000000000010",
             "The Holographic Principle",
             "Pretty easy stuff",
             384.2f,
-            null),
+            null,
+            new Event.Track[] {
+                new Event.Track("video/mp4", "https://example.com/holo-presenter.mp4", 24, 1280, 720),
+                new Event.Track("video/mp4", "https://example.com/holo-presentation.mp4", 24, 1920, 1080)
+            }),
         new Event("01234567-abcd-abcd-abcd-000000000011",
             "Finite Fields",
             null,
             534.7f,
-            null),
+            null,
+            new Event.Track[] {
+                new Event.Track("video/mp4", "https://example.com/math-presenter.mp4", 15, 800, 600),
+                new Event.Track("video/mp4", "https://example.com/math-presentation.mp4", 30, 1920, 1080)
+            }),
         new Event("01234567-abcd-abcd-abcd-000000000020",
             "Spring",
             "Doggo and Trees",
             464,
-            "76543210-abcd-abcd-abcd-000000000002"),
+            "76543210-abcd-abcd-abcd-000000000002",
+            new Event.Track[] {
+                new Event.Track("video/webm", "https://example.com/spring.webm", 23.97f, 2048, 858),
+            }),
         new Event("01234567-abcd-abcd-abcd-000000000021",
             "Cosmos Laundromaut",
             null,
             730,
-            "76543210-abcd-abcd-abcd-000000000002"),
+            "76543210-abcd-abcd-abcd-000000000002",
+            new Event.Track[] {
+                new Event.Track("video/webm", "https://example.com/cosmos.webm", 23.97f, 2048, 858),
+            }),
     };
 
     public static Series[] series = {
--- src/main/java/org/opencast/graphql/dummy/Event.java
+++ src/main/java/org/opencast/graphql/dummy/Event.java
@@ -8,13 +8,15 @@ public class Event {
     public String description;
     public float duration;
     public String seriesID;
+    public Track[] tracks;
 
-    public Event(String id, String title, String description, float duration, String seriesID) {
+    public Event(String id, String title, String description, float duration, String seriesID, Track[] tracks) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.duration = duration;
         this.seriesID = seriesID;
+        this.tracks = tracks;
     }
 
     public Series getSeries() {
@@ -22,5 +24,21 @@ public class Event {
             .filter(series -> series.id.equals(this.seriesID))
             .findFirst()
             .orElse(null);
+    }
+
+    public static class Track {
+        public String mimetype;
+        public String url;
+        public float framerate;
+        public int width;
+        public int height;
+
+        public Track(String mimetype, String url, float framerate, int width, int height) {
+            this.mimetype = mimetype;
+            this.url = url;
+            this.framerate = framerate;
+            this.width = width;
+            this.height = height;
+        }
     }
 }
